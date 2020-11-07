cf4c5c1fbd50340b8ab84431d4d24e2bdc08e27f
unknown <vuongphamvc@gmail.com>
2020-11-07 18:52:19 +0700

    update

--- configs/config_Header.js
+++ configs/config_Header.js
@@ -2,7 +2,6 @@ module.exports = [
     { name: "Home" , link: "/"},
     { name: "Product" , link: "/product"},
     { name: "Order" , link: "/order"},
-    { name: "Payment" , link: "/payment"},
     { name: "Report" , link: "/report"},
     { name: "Login" , link: "/login"},
     { name: "create User" , link: "/user/create"},
